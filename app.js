f4225eef7e26772490105e95a7b6262a958a151d
Justin MacAlpine <justin.macalpine@gmail.com>
2020-10-13 14:50:41 -0700

    Updates

---
+++ app.js
@@ -0,0 +1,118 @@
+// Target elements that will be used in a variety of places
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('registrar');
+  const input = form.querySelector('input');
+  const mainDiv = document.querySelector('.main');
+  const ul = document.getElementById('invitedList');
+
+  // Target filter checkbox elements
+  const div = document.createElement('div');
+  const filterLabel = document.createElement('label');
+  const filterCheckbox = document.createElement('input');
+
+  filterLabel.textContent = "Hide those who haven't responded";
+  filterCheckbox.type = 'checkbox';
+  div.appendChild(filterLabel);
+  div.appendChild(filterCheckbox);
+  mainDiv.insertBefore(div, ul);
+
+  // Change event listener to hide all attendees who haven't confirmed
+  filterCheckbox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if (isChecked) {
+      for (let i = 0; i < lis.length; i++) {
+        let li = lis[i];
+        if (li.className === 'responded') {
+          li.style.display = '';
+        } else {
+          li.style.display = 'none';
+        }
+      } 
+    } else {
+      for (let i = 0; i < lis.length; i++) {
+        let li = lis[i];
+        li.style.display = '';
+      }
+    }
+  });
+
+  // Function to create li elements
+  function createLI(text) {
+    function createElement(elementName, property, value) {
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
+    }
+
+    function appendToLI(elementName, property, value) {
+      const element = createElement(elementName, property, value);
+      li.appendChild(element);
+      return element;
+    }
+
+    const li = document.createElement('li');
+    appendToLI('span', 'textContent', text);
+    appendToLI('label', 'textContent', 'Confirmed')
+      .appendChild(createElement('input', 'type', 'checkbox'));
+    appendToLI('button', 'textContent', 'edit');
+    appendToLI('button', 'textContent', 'remove');
+    return li;
+  }
+
+  // Click event listener that calls createLI when form is submitted
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
+    ul.appendChild(li);
+  });
+
+  // Change event listener that changes the class on each li element when the checkbox is clicked
+  ul.addEventListener('change', (e) => {
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
+    if (checked) {
+      listItem.className = 'responded';
+    } else {
+      listItem.className = '';
+    }
+  });
+
+  // Click event listener on the edit button that allows you to change the text content of the respective li element
+  ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const button = e.target;
+      const li = button.parentNode;
+      const ul = li.parentNode;
+      const action = button.textContent;
+      const nameActions = {
+        remove: () => {
+          ul.removeChild(li);
+        },
+        edit: () => {
+          const span = li.firstElementChild;
+          const input = document.createElement('input');
+          input.type = "text";
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = 'save';
+        },
+        save: () => {
+          const input = li.firstElementChild;
+          const span = document.createElement('span');
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = 'edit';
+        }
+      };
+      // select and run action in button's name
+      nameActions[action]();
+    }
+  });
+});
+
